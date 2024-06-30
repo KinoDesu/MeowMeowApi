0a6b6bcaba93862a7bb6df15cacd2b789827d15a
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2024-06-30 16:33:18 -0300

    adiciona application-local.properties e faz busca de lista de produtos mockada

--- src/main/java/dev/kinodesu/MeowMeowApi/controller/ProductController.java
+++ src/main/java/dev/kinodesu/MeowMeowApi/controller/ProductController.java
@@ -1,14 +1,29 @@
 package dev.kinodesu.MeowMeowApi.controller;
 
+import dev.kinodesu.MeowMeowApi.model.Product;
+import dev.kinodesu.MeowMeowApi.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("product")
+@RequiredArgsConstructor
 public class ProductController {
-    
-    public void getProductList() {
-        
+
+    @Autowired
+    private final ProductService productService;
+
+    @GetMapping()
+    public ResponseEntity<?> getProductList() {
+        List<Product> productList = productService.listProducts();
+        return ResponseEntity.ok().body(productList);
     }
 
 }
--- src/main/java/dev/kinodesu/MeowMeowApi/model/shop/ShopProduct.java
+++
@@ -1,4 +0,0 @@
-package dev.kinodesu.MeowMeowApi.model.shop;
-
-public class ShopProduct {
-}
