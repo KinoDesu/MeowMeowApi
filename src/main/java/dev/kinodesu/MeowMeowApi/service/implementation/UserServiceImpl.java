09da379c6d118350cab8dc467b81c4f98c8e117b
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2024-08-13 19:17:06 -0300

    ajusta erro ao iniciar API

--- src/main/java/dev/kinodesu/MeowMeowApi/service/implementation/UserServiceImpl.java
+++ src/main/java/dev/kinodesu/MeowMeowApi/service/implementation/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package dev.kinodesu.MeowMeowApi.service.implementation;
 
 import dev.kinodesu.MeowMeowApi.repository.UserRepository;
+import dev.kinodesu.MeowMeowApi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 }
