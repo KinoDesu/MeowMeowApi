118e2d57e069155096f0fcfafa6754c275a00ac7
Leonardo <leonardo.fujimura123@gmail.com>
2024-08-27 14:16:54 -0300

    Adiciona springdoc no projeto

--- src/main/java/dev/kinodesu/MeowMeowApi/MeowMeowApiApplication.java
+++ src/main/java/dev/kinodesu/MeowMeowApi/MeowMeowApiApplication.java
@@ -1,9 +1,16 @@
 package dev.kinodesu.MeowMeowApi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "${spring.application.name}",
+		version = "${spring.application.version}",
+		description = "${spring.application.description}")
+)
 public class MeowMeowApiApplication {
 
 	public static void main(String[] args) {
